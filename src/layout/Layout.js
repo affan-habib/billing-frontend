34880777299b32f12169d5f255146dd744610df1
affan <affan@eatlbd.com>
2022-12-20 17:50:24 +0600

    update

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,158 +1,87 @@
 import * as React from "react";
-import PropTypes from "prop-types";
-import { Paper, Tabs, Tab, Typography, Box, Button, Fade } from "@mui/material";
-import Loader from "../components/Loader";
-import { LogoutOutlined } from "@mui/icons-material";
-import { useSelector } from "react-redux";
-import { selectApi } from "../reducers/apiSlice";
-const Billing = React.lazy(() => import("../pages/billing/Billing"));
-const Products = React.lazy(() => import("../pages/products/Products"));
-const Customers = React.lazy(() => import("../pages/customers/Customers"));
-const Sales = React.lazy(() => import("../pages/sales/Sales"));
-const About = React.lazy(() => import("../pages/about/About"));
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+import { styled } from "@mui/system";
+import TabsUnstyled from "@mui/base/TabsUnstyled";
+import TabsListUnstyled from "@mui/base/TabsListUnstyled";
+import TabPanelUnstyled from "@mui/base/TabPanelUnstyled";
+import { buttonUnstyledClasses } from "@mui/base/ButtonUnstyled";
+import TabUnstyled, { tabUnstyledClasses } from "@mui/base/TabUnstyled";
+import { Box } from "@mui/material";
+import Billing from "../pages/billing/Billing";
+const Tab = styled(TabUnstyled)`
+  font-family: IBM Plex Sans, sans-serif;
+  color: #fff;
+  cursor: pointer;
+  font-size: 0.875rem;
+  font-weight: 600;
+  background-color: transparent;
+  width: 100%;
+  padding: 10px 12px;
+  margin: 6px 6px;
+  border: none;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
 
+  &:hover {
+    background-color: gray;
+  }
+
+  &:focus {
+    color: #fff;
+  }
+
+  &.${tabUnstyledClasses.selected} {
+    background-color: #fff;
+    color: red;
+  }
+
+  &.${buttonUnstyledClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+const TabPanel = styled(TabPanelUnstyled)(
+  ({ theme }) => `
+  width: 100%;
+  font-family: IBM Plex Sans, sans-serif;
+  font-size: 0.875rem;
+  padding: 20px 12px;
+  background:  #fff;
+  border: 1px solid black;
+  `
+);
+
+const TabsList = styled(TabsListUnstyled)(
+  ({ theme }) => `
+  min-width: 400px;
+  background-color: tomato;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-content: space-between;
+  max-width: 600px;
+  `
+);
+
+export default function UnstyledTabsIntroduction() {
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
-
-export default function Layout() {
-  const { loading } = useSelector(selectApi);
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <React.Suspense fallback={<Loader />}>
-      {loading && <Loader />}
-      <Box>
-        <Box>
-          <Box
-            sx={{
-              borderBottom: 1,
-              borderColor: "divider",
-              background: "#f5f9f0",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              alignItems: "space-between",
-            }}
-          >
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              aria-label="basic tabs example"
-            >
-              <Tab
-                disableRipple
-                disableFocusRipple
-                disableTouchRipple
-                label="NEW BILL"
-                {...a11yProps(0)}
-              />
-              <Tab
-                disableRipple
-                disableFocusRipple
-                disableTouchRipple
-                label="SALES"
-                {...a11yProps(1)}
-              />
-              <Tab
-                disableRipple
-                disableFocusRipple
-                disableTouchRipple
-                label="CUSTOMERS"
-                {...a11yProps(2)}
-              />
-              <Tab
-                disableRipple
-                disableFocusRipple
-                disableTouchRipple
-                label="ITEMS"
-                {...a11yProps(3)}
-              />
-              <Tab
-                disableRipple
-                disableFocusRipple
-                disableTouchRipple
-                label="ABOUT"
-                {...a11yProps(4)}
-              />
-              <Button
-                startIcon={<LogoutOutlined />}
-                variant="outlined"
-                color="error"
-                sx={{
-                  ml: "auto",
-                  mr: 2,
-                  height: 30,
-                  my: "auto",
-                  borderRadius: 10,
-                }}
-                onClick={() => {
-                  localStorage.removeItem("token");
-                  window.location.reload();
-                }}
-              >
-                LOGOUT
-              </Button>
-            </Tabs>
-          </Box>
-          <TabPanel value={value} index={0}>
-            <Paper sx={{ minHeight: "93vh", bgcolor: "#e2ffff" }}>
-              <Billing />
-            </Paper>
-          </TabPanel>
-          <TabPanel value={value} index={1}>
-            <Paper sx={{ padding: 2, bgcolor: "#e2ffff", minHeight: "93vh" }}>
-              <Sales />
-            </Paper>
-          </TabPanel>
-          <TabPanel value={value} index={2}>
-            <Paper sx={{ padding: 2, bgcolor: "#e2ffff", minHeight: "93vh" }}>
-              <Customers />
-            </Paper>
-          </TabPanel>
-          <TabPanel value={value} index={3}>
-            <Paper sx={{ padding: 2, bgcolor: "#e2ffff", minHeight: "93vh" }}>
-              <Products />
-            </Paper>
-          </TabPanel>
-          <TabPanel value={value} index={4}>
-            <Paper sx={{ p: 2, bgcolor: "#e2ffff", minHeight: "93vh" }}>
-              <About />
-            </Paper>
-          </TabPanel>
-        </Box>
+    <TabsUnstyled defaultValue={0}>
+      <Box sx={{ mx: "auto", width: "100%", py: 1, bgcolor: "green" }}>
+        <TabsList sx={{ mx: "auto" }}>
+          <Tab>Billing</Tab>
+          <Tab>Sales</Tab>
+          <Tab>Customers</Tab>
+          <Tab>Items</Tab>
+          <Tab>About</Tab>
+        </TabsList>
       </Box>
-    </React.Suspense>
+      <TabPanel value={0}>
+        <Billing />
+      </TabPanel>
+      <TabPanel value={1}>Profile page</TabPanel>
+      <TabPanel value={2}>Language page</TabPanel>
+    </TabsUnstyled>
   );
 }
